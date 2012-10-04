a99c7c3b913b377000db1049099bf2a7efc424e2
cdubose <cdubose@marlin.(none)>
2012-10-04 09:25:11 -0500

    cleaning up roles

--- db/fixtures/roles.rb
+++ db/fixtures/roles.rb
@@ -2,8 +2,6 @@ app = App.find_by_name('epi')
 
 Role.find_or_create_by_name_and_app_id("Public",app.id){|r| r.attributes = {:user_role => true, public: true} }
 Role.find_or_create_by_name_and_app_id("Admin",app.id){|r| r.attributes = {:user_role => false} }
-Role.find_or_create_by_name_and_app_id("Epidemiologist",app.id){|r| r.attributes = {:user_role => true} }
-Role.find_or_create_by_name_and_app_id("Health Officer",app.id){|r| r.attributes = {:user_role => true} }
 # TODO
 # Integrate school selection into roles and/or user
 
@@ -17,7 +15,3 @@ u.role_memberships.create(
   :jurisdiction_id => Jurisdiction.find_by_name('Harris').id,
   :role_id => Role.find_by_name('Public').id
 ) if Role.find_by_name('Public')
-u.role_memberships.create(
-  :jurisdiction_id => Jurisdiction.find_by_name('Texas').id,
-  :role_id => Role.superadmin("epi").id
-)
